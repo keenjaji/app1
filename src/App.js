871b4b768ad527f7ac528cfd50fa2bb4389e1711
keenjaji <110646868+keenjaji@users.noreply.github.com>
2022-10-07 10:51:34 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import FormBootstrap from './form-Bootstrap'
 import FormText from'./form-text';
+import Formalidation from'./form-validation';
 
 export default function App() {
 /*return <FormBootstrap/>
 */
-return<FormText/>
+return<FormValidation/>
 }
